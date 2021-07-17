8e097a13a40c31ca50884943ba370f3395f90d27
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-07-18 00:47:36 +0900

    setting tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlack: '#37393b',
+        customColor: colors.red,
+      },
+    },
   },
   variants: {
     extend: {},
