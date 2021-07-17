546704fdd906a085976ee19c8415743369f72fb1
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-07-18 00:53:50 +0900

    update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Header from '../components/organisms/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Portfolio</title>
+        <meta name="description" content="This is my portfolio site" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
