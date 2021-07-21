ad74718263d3cd28f57b212ce31ca2a85ee3e2d7
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-07-21 15:33:05 +0900

    fix #12

--- src/components/organisms/SidebarNavigation.tsx
+++ src/components/organisms/SidebarNavigation.tsx
@@ -3,7 +3,7 @@ import SidebarNavigationItem from '../atoms/SidebarNavigationItem';
 
 const SidebarNavigation: React.VFC = () => {
   return (
-    <nav className="h-full flex flex-row sm:flex-col justify-around sm:justify-start pb-12 sm:pr-12 sm:pb-0">
+    <nav className="flex flex-row sm:flex-col sm:h-full justify-around sm:justify-start pb-12 sm:pr-12 sm:pb-0">
       <SidebarNavigationItem ItemName={'portfolio'} URL={'/'} />
       <SidebarNavigationItem ItemName={'about'} URL={'/about'} />
       <SidebarNavigationItem ItemName={'contact'} URL={'/contact'} />
--- src/constants/Text.tsx
+++ src/constants/Text.tsx
@@ -1,5 +1,6 @@
 export const Text = {
   portfolioIntro:
     'Hi,this is my portfolio site. dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text',
+  shortPortfolioIntro: 'Hi,this is my portfolio site.',
   explain: 'dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text dummy text',
 };
