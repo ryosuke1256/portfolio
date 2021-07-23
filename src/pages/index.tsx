c33a51d460082ac6f7487750ae0edef202499bb9
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-07-23 21:42:32 +0900

    fix env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,8 @@ import { Client } from '../types/index';
 
 export const getStaticProps: GetStaticProps = async () => {
   const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
   } as Client);
 
   try {
