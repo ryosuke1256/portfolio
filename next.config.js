0e6f32ae73015199b4c7462c86f6da4100825e88
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-06-04 16:00:54 +0900

    next.config.jsリファクタリング

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
-module.exports = {
-  reactStrictMode: true,
-};
-
 const withPWA = require('next-pwa');
 
-module.exports = withPWA({
+// https://github.com/shadowwalker/next-pwa
+// https://developer.chrome.com/docs/workbox/
+https: module.exports = withPWA({
   pwa: {
     dest: 'public', // swの出力ディレクトリ
+    register: true, // default true
+    skipWaiting: true, //default true, Service Worker の更新が即座に有効になる
     // runtimeCaching: []
     disable: process.env.NODE_ENV === 'development',
   },
-});
-
-module.exports = {
   images: {
     domains: ['images.ctfassets.net'],
   },
-};
+  reactStrictMode: true,
+});
