9c64555bf7e1d20b1cec3e1c486fd6e3f0d720a7
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-06-14 19:28:49 +0900

    画像ホバー時に矢印を表示する

--- src/components/organisms/Portfolio.tsx
+++ src/components/organisms/Portfolio.tsx
@@ -1,34 +1,42 @@
-import type { FC } from 'react';
+import { FC, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import type { Entry } from 'contentful';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import type { IPortfoliosFields } from '../../../@types/generated/contentful';
 import clsx from 'clsx';
+import Arrow from '/public/images/arrow_up_right.svg';
 
 const img = {
   height: 150,
   width: 150,
 };
 
-const imageClass = 'w-full max-w-sm text-center rounded-3xl bg-white p-2 md:self-center';
+const imageClass = 'w-full max-w-sm text-center rounded-3xl bg-white p-2 relative md:self-center';
 
-type Props = {
+type PortfolioProps = {
   portfolio: Entry<IPortfoliosFields>;
 };
 
-export const Portfolio: FC<Props> = ({ portfolio }: Props) => {
+export const Portfolio: FC<PortfolioProps> = ({ portfolio }: PortfolioProps) => {
+  const [isShowArorw, setIsShowArrow] = useState<boolean>(false);
   const { title, link, image, alt, explain, technology } = portfolio.fields;
   const url = image.fields.file.url;
 
   return (
     <>
-      {/* TODO: 画像hoverした時に遷移できることを伝えるなら右上に矢印とか合ったほうがいい */}
       <div className='pb-12 flex flex-col items-center md:w-1/2 md:pr-10 md:even:pr-0 md:even:pl-10 md:items-start'>
         {typeof link === 'string' ? (
           <Link href={link}>
-            <a target='_blank' rel='noopener noreferrer' className={clsx(imageClass, 'hover:scale-105 hover:shadow-md cursor-pointer')}>
+            <a
+              target='_blank'
+              onMouseOver={() => setIsShowArrow(true)}
+              onMouseLeave={() => setIsShowArrow(false)}
+              rel='noopener noreferrer'
+              className={clsx(imageClass, 'hover:scale-105 hover:shadow-md cursor-pointer')}
+            >
               <Image src={`https:${url}`} alt={alt} layout='intrinsic' height={img.height} width={img.width} />
+              {isShowArorw && <Arrow className='absolute top-2 right-2' fill='#d9d9d9' height={24} width={24} />}
             </a>
           </Link>
         ) : (
